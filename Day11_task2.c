b85f2c1766ddc60159b084ff29ada76e08b87833
Kartik-Ingole <147076368+Kartik-Ingole@users.noreply.github.com>
2023-10-16 08:46:17 +0530

    #Day-11-Task-2
    Program File

---
+++ Day11_task2.c
@@ -0,0 +1,18 @@
+/*Day 11 (10/16/2023) Task 2
+Display Factors of a Number*/
+
+#include <stdio.h>
+int main() {
+    int num, i;
+    printf("Enter a positive integer: ");
+    scanf("%d", &num);
+    printf("Factors of %d are: ", num);
+    for (i = 1; i <= num; ++i) {
+        if (num % i == 0) {
+            printf("%d ", i);
+        }
+    }
+    return 0;
+}
+
+//KARTIK INGOLE
